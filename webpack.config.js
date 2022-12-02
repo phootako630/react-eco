625c8c6ab57f359839dcd1a3f4070faf27dcf54d
ivanzhang <ivanzhang220820@gmail>
2022-12-01 23:22:21 -0500

    basic config of webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"]}
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: "bundle.js"
+    },
+    devServer: {
+        static : {
+            directory : path.join(__dirname, "public/")
+        },
+        port: 3000,
+        //publicPath: 'http://localhost:3000/dist/',
+        devMiddleware:{
+            publicPath: "https://localhost:3000/dist/",
+        },
+        hot: "only"
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+}
