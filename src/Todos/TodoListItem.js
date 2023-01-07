263a2c54422c3fe2f4f6558b43e900e2541e7ae2
ivanzhang <ivanzhang220820@gmail>
2023-01-07 10:28:46 -0500

    Added Mocha test for functionality

--- src/Todos/TodoListItem.js
+++ src/Todos/TodoListItem.js
@@ -10,12 +10,14 @@ const TodoItemContainer = styled.div`
   position: relative;
   box-shadow: 0 4px 8px grey;
 `;
+export const getBorderStyleForDate = (startingDate, currentDate) =>
+    (startingDate > new Date(currentDate - 8640000 * 5)
+            ? 'none'
+            : '2px solid red');
+
 
 const TodoItemContainerWithWarning = styled(TodoItemContainer)`
-  border-bottom: ${props => (new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
-                  ? 'none'
-                  : '2px solid red'
-  )};
+  border-bottom: ${props => getBorderStyleForDate(new Date(props.createdAt), Date.now())};
 `;
 
 const Button = styled.button`
